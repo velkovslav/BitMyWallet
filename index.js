281243de08b288c12ec06d0555463cce52e3f03f
Slave <velkov13@gmail.com>
2019-08-16 10:01:54 +0200

    Fixed the loading bug, added a response to app.post

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // Packages
 const express = require('express');
 const bodyParser = require('body-parser');
-//
+// End of Packages
 
-var app = express();
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Get the root index.html file
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
 
-
+// Get the variables form the converter form
+app.post("/",function(req,res){
+    var crypto = req.body.crypto;
+    var currency = req.body.currency;
+    res.send('You have choosen ' + crypto + ' to be converted in ' + currency);
+    console.log(crypto);
+});
 
 // Start the server
 app.listen(3000, function(){
