588697b99c85d839705edee5495cbb6434e4812d
Slave <velkov13@gmail.com>
2019-08-20 14:06:08 +0200

    Made API call parameters, added amount field in html

--- index.js
+++ index.js
@@ -16,22 +16,28 @@ app.get("/", function(req,res){
 app.post("/",function(req,res){
     var crypto = req.body.crypto; //Example output BTC
     var currency = req.body.currency; //Example output USD
+    var amount = req.body.amount;
 
-    // Start of API Request for crypto conversion
-    var baseUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+    var options = {
+        url: 'https://apiv2.bitcoinaverage.com/convert/global',
+        method: 'POST',
+        qs: {
+            from: crypto,
+            to: currency,
+            amount: amount
+        }
+    };
+    
+    request(options,function (error,respond,body) {
+        console.log(options)
+        // res.send('Okay')
 
-    request(baseUrl + crypto + currency, function (error, response, body){
-        data = JSON.parse(body);
-        currentPrice = Math.round(data.ask);
-        res.send('<h1>' + currentPrice + ' ' + currency + '</h1>');
-    });
-
-
-
+    })
 
 });
 
 // Start the server
+
 app.listen(3000, function(){
    console.log('The server is working on port 3000');
 });
