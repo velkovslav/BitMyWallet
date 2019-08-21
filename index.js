2d058c97e5a6a5aadbff39aef84943c94aa5ad58
Slave <velkov13@gmail.com>
2019-08-21 12:51:14 +0200

    The request for the converter API is created.

--- index.js
+++ index.js
@@ -13,27 +13,29 @@ app.get("/", function(req,res){
 });
 
 // Get the variables form the converter form
-app.post("/",function(req,res){
+app.post("/",function(req,res) {
     var crypto = req.body.crypto; //Example output BTC
     var currency = req.body.currency; //Example output USD
     var amount = req.body.amount;
 
     var options = {
         url: 'https://apiv2.bitcoinaverage.com/convert/global',
-        method: 'POST',
+        method: 'GET',
         qs: {
             from: crypto,
             to: currency,
-            amount: amount
+            amount: amount,
         }
     };
-    
-    request(options,function (error,respond,body) {
-        console.log(options)
-        // res.send('Okay')
 
+    request(options, function (error, respond, body) {
+        var data = JSON.parse(body);
+        var time = data.time;
+        var price = data.price;
+        var priceWithDecimal = Math.round(price);
+        console.log(data);
+        res.send('The conversion rate is ' + price + ' ' + currency + ' ' + time);
     })
-
 });
 
 // Start the server
